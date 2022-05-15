f9a9eefe78c6b926a5d5fe1c2a4b3de089461c39
ayham <altffour@protonmail.com>
2022-05-15 06:38:45 +0300

    fix minor bug

--- assets/apps/eid.marks/app.js
+++ assets/apps/eid.marks/app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fbfd257e1e53e15e2189bb15cdf85845e1933a6300e93498c31f3debffcc7c2
-size 6883
+oid sha256:01c7d3eeac615df4f53677f434195cbf355f0a0821e2659b2fc52ec251b60cfe
+size 6862
